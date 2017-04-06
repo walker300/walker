468eb88fc962faf1d077a59036144ef5ef699478
walker300 <wqrkyt@gmail.com>
2017-04-06 10:03:28 -0400

    Update music.js

--- music.js
+++ music.js
@@ -1,1 +1,45 @@
+//finish this and add to typed.js
 
+
+
+
+
+  var randomnumber = Math.round(Math.random() * 5);
+	var audio0 = new Audio('da.mp3');
+	var audio1 = new Audio('wing.mp3');
+	var audio2 = new Audio('mall.mp3');
+	var audio3 = new Audio('carti.mp3');
+	var audio4 = new Audio('skimask.mp3');
+	var audio5 = new Audio('youngma.mp3');
+	var $song 
+	
+	var audio6 = new Audio('skimask.mp3');
+	audio6.play();
+		
+	switch(l) {
+	case 0:
+        	audio1.play();
+          $song = ""
+        	break;
+	case 1:
+        	audio2.play();
+          $song = ""
+        	break;
+	case 2:
+        	audio3.play();
+          $song = ""
+        	break;
+	case 3:
+        	audio4.play();
+          $song = ""
+        	break;
+	case 4:
+        	audio5.play();
+          $song = ""
+        	break;
+	case 5:
+		      audio0.play();
+          $song = ""
+		      break;
+		
+			   }
